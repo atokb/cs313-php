af3eae1ccf0d297c3a1bdb8eabaacabe5481c939
atokb <ato15001@byui.edu>
2019-05-04 18:31:49 -0600

    added files

---
+++ web/header.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Welcome to Ato Brown's Page</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link <?php if($currentPage == 'home') echo 'bg-info'; ?>" href="home.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?php if($currentPage == 'assignments') echo 'bg-info'; ?>" href="assignments.php">Assignments</a>
+      </li>
+    </ul>
+  </div>
+</nav>
