8e1ff205f8d4ac4603405f627d893e751439aaba
atokb <ato15001@byui.edu>
2019-05-04 18:39:12 -0600

    added files

--- web/home.php
+++ web/home.php
@@ -9,13 +9,14 @@ $currentPage = 'home';
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="main.css">
   <title>Ato Brown's Page</title>
 </head>
 <body>
   <?php
     include('header.php');
   ?>
-  <div>
+  <div class="jumbotron">
       <h1>Welcome to Ato Brown's Page</h1>
       <div>
         <img src="" alt="">
