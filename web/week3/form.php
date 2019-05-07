aed82c73a2627de75b73768207683b976df762ed
atokb <ato15001@byui.edu>
2019-05-07 15:50:40 -0600

    img

--- web/week3/form.php
+++ web/week3/form.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>PHP Hackz</title>
 </head>
 <body>
+<div class="jumbotron">
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <legend>
     <h1>Leave a Review</h1>
@@ -20,9 +22,10 @@
     </div>
     <div>
     <label for="major">Major: </label>
-    <input type="text" name="major">
+    <input type="radio" name="major" value="compScience">
     </div>
     </legend>
     </form>
+</div>
 </body>
 </html>
