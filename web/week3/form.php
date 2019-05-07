f966d91eda52fb417314bf6dc5bb6a5bfecb1531
atokb <ato15001@byui.edu>
2019-05-07 16:43:28 -0600

    img

--- web/week3/form.php
+++ web/week3/form.php
@@ -22,6 +22,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $major = test_input($_POST["major"]);
   }
 
+  if (empty($_POST["places"])) {
+    $majorErr = "Places is required";
+  } else {
+    $major = test_input($_POST["places"]);
+  }
+
   if (empty($_POST["comments"])) {
     $comments = "";
   } else {
@@ -46,42 +52,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <legend>
     <h1>Leave a Review</h1>
     <div class=form-group>
-    <label for="name" >Name: </label>
-    <input type="text" name="name" class="form-control">
+    <label for="name">Name: </label>
+    <input type="text" name="name" class="form-control" value="<?php echo $name ?>">
     </div>
     <div class=form-group>
-    <label for="email" >Email: </label>
-    <input type="text" name="email" class="form-control">
+    <label for="email">Email: </label>
+    <input type="text" name="email" class="form-control" value="<?php echo $email ?>">
     </div>
     <div>
     <hr>
     <h4>Major</h4>
     <label for="major">Computer Science </label>
-    <input type="radio" name="major" value="compScience"><br>
+    <input type="radio" name="major" value="compScience" <?php if (isset($major) && $major=="compScience") echo "checked";?>><br>
     <label for="major">Web Design and Development </label>
-    <input type="radio" name="major" value="wdd"><br>
+    <input type="radio" name="major" value="wdd" <?php if (isset($major) && $major=="wdd") echo "checked";?>><br>
     <label for="major">Computer Information Technology </label>
-    <input type="radio" name="major" value="compTech"><br>
+    <input type="radio" name="major" value="compTech" <?php if (isset($major) && $major=="compTech") echo "checked";?>><br>
     <label for="major">Computer Engineering </label>
-    <input type="radio" name="major" value="compEngineering">
+    <input type="radio" name="major" value="compEngineering" <?php if (isset($major) && $major=="compEngineering") echo "checked";?>>
     </div>
     <hr>
     <div>
     <h4>Places Visited</h4>
     <label for="places">North America</label>
-    <input type="checkbox" name="continent" value="northAmerica"><br>
+    <input type="checkbox" name="continent" value="northAmerica" <?php if (isset($places) && $places=="northAmerica") echo "checked";?>><br>
     <label for="places">South America</label>
-    <input type="checkbox" name="continent" value="southAmerica"><br>
+    <input type="checkbox" name="continent" value="southAmerica" <?php if (isset($places) && $places=="southAmerica") echo "checked";?>><br>
     <label for="places">Europe</label>
-    <input type="checkbox" name="continent" value="europe"><br>
+    <input type="checkbox" name="continent" value="europe" <?php if (isset($places) && $places=="europe") echo "checked";?>><br>
     <label for="places">Asia</label>
-    <input type="checkbox" name="continent" value="asia"><br>
+    <input type="checkbox" name="continent" value="asia" <?php if (isset($places) && $places=="asia") echo "checked";?>><br>
     <label for="places">Australia</label>
-    <input type="checkbox" name="continent" value="australia"><br>    
+    <input type="checkbox" name="continent" value="australia" <?php if (isset($places) && $places=="australia") echo "checked";?>><br>    
     <label for="places">Africa</label>
-    <input type="checkbox" name="continent" value="africa"><br>
+    <input type="checkbox" name="continent" value="africa" <?php if (isset($places) && $places=="africa") echo "checked";?>><br>
     <label for="places">Antarctica</label>
-    <input type="checkbox" name="continent" value="antarctica">
+    <input type="checkbox" name="continent" value="antarctica" <?php if (isset($places) && $places=="antarctica") echo "checked";?>>
     </div>
     <hr>
     <div>
