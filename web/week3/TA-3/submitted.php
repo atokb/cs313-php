c5fe7a34d0e03b5183e8ec207f76ec0a9d0f0ff4
atokb <ato15001@byui.edu>
2019-05-07 17:21:08 -0600

    img

--- web/week3/TA-3/submitted.php
+++ web/week3/TA-3/submitted.php
@@ -36,5 +36,17 @@ if {
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <p>Name: <? echo $name ?></p>
 <p>Where you've visited <?echo $continent ?></p>
+</body>
+</html>
+
